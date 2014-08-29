a27d54d95f58034c06b9fb8ecaa9b70e21b03b38
Luan Cestari <luan.cestari@gmail.com>
2014-08-29 10:49:47 -0300

    Fixes

--- crud/src/test/java/com/redhat/lightblue/crud/rdbms/ITCaseRDBMSCRUDControllerTest.java
+++ crud/src/test/java/com/redhat/lightblue/crud/rdbms/ITCaseRDBMSCRUDControllerTest.java
@@ -27,7 +27,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 
-public class RDBMSCRUDControllerTest {
+public class ITCaseRDBMSCRUDControllerTest {
 
     public static DataSource dsMock = null;
     public static Connection cMock = null;
