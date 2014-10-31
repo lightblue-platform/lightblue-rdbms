27f1c765237ec1122ed70888aad4b23d64d9b4c8
Dennis Crissman <dcrissma@redhat.com>
2014-10-31 15:56:45 -0400

    bulk up unit tests

--- metadata/src/main/java/com/redhat/lightblue/metadata/rdbms/impl/RDBMSDataStoreParser.java
+++ metadata/src/main/java/com/redhat/lightblue/metadata/rdbms/impl/RDBMSDataStoreParser.java
@@ -18,9 +18,9 @@
  */
 package com.redhat.lightblue.metadata.rdbms.impl;
 
+import com.redhat.lightblue.common.rdbms.RDBMSConstants;
 import com.redhat.lightblue.common.rdbms.RDBMSDataStore;
 import com.redhat.lightblue.metadata.DataStore;
-import com.redhat.lightblue.common.rdbms.RDBMSConstants;
 import com.redhat.lightblue.metadata.parser.DataStoreParser;
 import com.redhat.lightblue.metadata.parser.MetadataParser;
 
@@ -35,7 +35,7 @@ public class RDBMSDataStoreParser<T> implements DataStoreParser<T> {
     public static final String NAME = "rdbms";
 
     @Override
-    public DataStore parse(String name, MetadataParser<T> p, T node) {
+    public RDBMSDataStore parse(String name, MetadataParser<T> p, T node) {
         if (!NAME.equals(name)) {
             throw com.redhat.lightblue.util.Error.get(RDBMSConstants.ERR_ILL_FORMED_METADATA, name);
         }
